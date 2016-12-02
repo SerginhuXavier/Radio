e3645671a11896c8a9199def5316f88a0cb88f79
Paulo Sergio <paulo.sergio@agenciaguppy.com.br>
2016-12-02 03:15:23 -0200

    first commit

---
+++ app/src/main/java/br/com/paulosergioxavier/radio/MusicController.java
@@ -0,0 +1,15 @@
+package br.com.paulosergioxavier.radio;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Serginhu on 02/12/2016.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+
+    public void hide(){}
+}
